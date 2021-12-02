aebd9836bcca0f393fda1ec931cef4e505224e4d
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2021-12-02 19:02:31 +0100

    player move 1-2-3

--- js/script.js
+++ js/script.js
@@ -19,3 +19,24 @@ else if(randomNumber == 3){
 }
 printMessage('Mój ruch to: ' + computerMove);
 ​
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(playerInput == '2'){
+  playerMove = 'papier';
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
