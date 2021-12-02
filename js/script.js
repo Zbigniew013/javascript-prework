51341e4c7995bd02128d3f98770c751b34354060
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2021-12-02 19:37:34 +0100

    full game

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
+
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -40,3 +41,45 @@ else if(playerInput == '3'){
   playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyczki'){
+  printMessage('Ja wygrywam!')
+}
+else if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Nie wybrałeś kamienia, papieru ani nożyczek');
+}
+
+if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ja wygrywam!');
+}
+else if( computerMove == 'papier' && playerMove == 'nożyczki'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+  printMessage('Nie wybrałeś kamienia, papieru ani nożyczek');
+}
+
+
+if( computerMove == 'nożyczki' && playerMove == 'papier'){
+  printMessage('Ja wygrywam!');
+}
+else if( computerMove == 'nożyczki' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'nożyczki' && playerMove == 'nożyczki'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'nożyczki' && playerMove == 'nieznany ruch'){
+  printMessage('Nie wybrałeś kamienia, papieru ani nożyczek');
+}
