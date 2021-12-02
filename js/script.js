c647cfdd854e238c07f14f9cc9759209c671dbee
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2021-12-02 18:57:17 +0100

    computer move 1-2-3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-let playerMove = 'papier';
-printMessage('Zagrałem' + playerMove + '! Jeśli Twoj ruch to nożyce, to wygrywasz');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+​
+console.log('Wylosowana liczba to: ' + randomNumber);
+​
+let computerMove = 'nieznany ruch';
+​
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+printMessage('Mój ruch to: ' + computerMove);
+​
+else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+printMessage('Mój ruch to: ' + computerMove);
+​
+else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
+​
